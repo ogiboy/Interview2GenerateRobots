4c6accce1a9ebf99aeb828bde28c238b7d825d9c
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-04-29 03:59:50 +0300

    update fetchRobots

--- src/context/robots.js
+++ src/context/robots.js
@@ -1,57 +1,70 @@
-import axios from "axios";
-import { createContext, useState } from "react";
+import axios from 'axios'
+import { createContext, useState } from 'react'
 
-const RobotsContext = createContext();
+const RobotsContext = createContext()
 
 const Provider = ({ children }) => {
-  const [text, setText] = useState("");
-  const [robots, setRobots] = useState([]);
+  const [text, setText] = useState('')
+  const [robots, setRobots] = useState([])
 
   const fetchRobots = async () => {
     try {
       const response = await axios.get(`https://robohash.org/${text}`, {
-        responseType: "arraybuffer"
-      });
-      const imgData = Buffer.from(response.data).toString("base64");
-      const imgSrc = `data:image/png;base64,${imgData}`;
+        responseType: 'arraybuffer',
+      })
+      // console.log(response)
+      // const imgData = Buffer.from(response.data).toString("base64");
+      // const imgSrc = `data:image/png;base64,${Buffer.from(
+      //   response.data
+      // ).toString('base64')}`
 
-      const robotName = text.toLowerCase();
+      // const base64ImageString = Buffer.from(response.data, 'binary')
+      // const imgSrc = 'data:image/png,base64,' + base64ImageString
 
-      if (isRobotAlreadyThere(robotName)) {
-        alert("Robot is already in the list!");
-      } else {
-        setRobots((prevRobots) => [
-          ...prevRobots,
-          { id: crypto.randomUUID(), img: imgSrc, name: robotName }
-        ]);
+      const blob = new Blob([response.data], { type: 'image/png' })
+
+      const reader = new FileReader()
+      reader.readAsDataURL(blob)
+      reader.onloadend = () => {
+        const imgSrc = reader.result
+        const robotName = text.toLowerCase()
+
+        if (isRobotAlreadyThere(robotName)) {
+          alert('Robot is already in the list!')
+        } else {
+          setRobots((prevRobots) => [
+            ...prevRobots,
+            { id: crypto.randomUUID(), img: imgSrc, name: robotName },
+          ])
+        }
       }
     } catch (error) {
-      console.warn("Error fetching robots:", error);
+      console.warn('Error fetching robots:', error)
     }
-  };
+  }
 
   const handleForm = (e) => {
-    e.preventDefault();
-    fetchRobots();
-    setText("");
-  };
+    e.preventDefault()
+    fetchRobots()
+    setText('')
+  }
 
   const handleDelete = (id) => {
-    setRobots((prevRobots) => prevRobots.filter((robo) => robo.id !== id));
-  };
+    setRobots((prevRobots) => prevRobots.filter((robo) => robo.id !== id))
+  }
 
   const isRobotAlreadyThere = (name) => {
-    return robots.some((robot) => robot.name === name);
-  };
+    return robots.some((robot) => robot.name === name)
+  }
 
-  const valuesToShare = { text, setText, handleForm, robots, handleDelete };
+  const valuesToShare = { text, setText, handleForm, robots, handleDelete }
 
   return (
     <RobotsContext.Provider value={valuesToShare}>
       {children}
     </RobotsContext.Provider>
-  );
-};
+  )
+}
 
-export { Provider };
-export default RobotsContext;
+export { Provider }
+export default RobotsContext
